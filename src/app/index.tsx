48278e9b9bba6792e1d4229f5bf9c8291e77166d
Pedro Nest <pedronestjp@gmail.com>
2024-02-09 16:56:05 -0300

    render category-button with flatList

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,18 +1,24 @@
-import { View } from "react-native";
+import { View, FlatList } from "react-native";
 import { Header } from "@/components/header";
 import { CategoryButton } from "@/components/category-button";
 
+import { CATEGORIES } from "@/utils/data/products";
+
 export default function Home() {
-    return(
+    return (
         <View className="flex-1 pt-8">
 
             <Header title={'Faça seu pedido'} cartQuantityItems={2} />
 
-            <View className="flex-row gap-4">
-                <CategoryButton title="Lanches do dia" />
-                <CategoryButton title="Lanches do dia" isSelected/>
-                <CategoryButton title="Lanches do dia" />
-            </View>
+            <FlatList 
+            data={CATEGORIES}
+            keyExtractor={(item) => item}
+            renderItem={({item}) => <CategoryButton title={item}/>}
+            horizontal
+            className="max-h-10 mt-5"
+            contentContainerStyle={{gap:12, paddingHorizontal:5}}
+            showsHorizontalScrollIndicator={false}
+            />
 
         </View>
     )
