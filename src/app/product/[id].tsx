a8f1099435ee30b079977b6e1b2453f9582c318e
Pedro Nest <pedronestjp@gmail.com>
2024-02-12 11:47:23 -0300

    product screen in progress

--- src/app/product/[id].tsx
+++ src/app/product/[id].tsx
@@ -1,16 +1,24 @@
-import { View } from "react-native";
+import { Image, Text, View } from "react-native";
 import { useLocalSearchParams } from "expo-router";
 import { PRODUCTS } from "@/utils/data/products";
 
 export default function Product() {
 
     const { id } = useLocalSearchParams()
-    const product = PRODUCTS.filter((item) => item.id == id)
+    const product = PRODUCTS.filter((item) => item.id == id)[0]
     console.log(product)
 
     return (
         <View className="flex-1">
-            
+            <Image
+                source={product.cover}
+                className="w-full h-52"
+                resizeMode="cover" />
+            <View className="p-5 ">
+                <Text className="font-heading text-lg text-lime-400">
+                    {product.price}
+                </Text>
+            </View>
         </View>
     )
 }
